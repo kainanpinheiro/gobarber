77ac73bf4e579f26ae93ce8b0323b9d3229d5b23
kainanpinheiro <kainan.silva@aluno.faculdadeimpacta.com.br>
2020-10-22 14:12:30 -0300

    first commit

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd2cc345e208e4695bb5f21f0cd620f42',
+  expiresIn: '7d',
+};
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '438d9a5cf811f6',
+    pass: 'fc10de45f7a323',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
