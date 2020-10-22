9a567e2d467fb83d11c872c08b55c860a95ba47e
kainanpinheiro <kainan.silva@aluno.faculdadeimpacta.com.br>
2020-10-22 16:00:56 -0300

    variaveis de ambiente e algumas alteracoes

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'd2cc345e208e4695bb5f21f0cd620f42',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '438d9a5cf811f6',
-    pass: 'fc10de45f7a323',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
